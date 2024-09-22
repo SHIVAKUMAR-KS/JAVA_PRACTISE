8496cd1f3d5d920a1ae73cd3a9776ca52cdd9bc7
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-22 23:47:52 +0530

    added

--- JAVA/OOPS/CLASS/Car.java
+++ JAVA/OOPS/CLASS/Car.java
@@ -3,6 +3,7 @@ package Class;
 public class Car {
     public static void main(String[] args) {
         System.out.println("Will start to work on java project based on oops");
+        System.out.println("Will start to work on java project based on oops");
     }
     
 }
