4a023f33f4949d7f44806e7f3856713c28a8d69c
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-23 22:35:58 +0530

    added some code in class based on student object

--- JAVA/OOPS/CLASS/Car.java
+++
@@ -1,9 +0,0 @@
-package Class;
-
-public class Car {
-    public static void main(String[] args) {
-        System.out.println("Will start to work on java project based on oops");
-        System.out.println("Will start to work on java project based on oops");
-    }
-    
-}
---
+++ JAVA/OOPS/Class_Section/main.java
@@ -0,0 +1,40 @@
+package Class_Section;
+
+class Student{
+    public String name;
+    public int age;
+    public String StudentId;
+
+    //constructor
+    public Student(String name,int age,String StudentId){
+        this.name=name;
+        this.age=age;
+        this.StudentId=StudentId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getStudentId(){
+        return StudentId;
+    }
+
+    public void display(){
+        System.out.println("name :"+name);
+        System.out.println("age :"+age);
+        System.out.println("Student id is : "+StudentId);
+    }
+
+}
+public class main {
+    public static void main(String[] args) {
+        Student st =new Student("shiva",20,"05J2");
+
+        Student st1=new Student("Ayush",23,"05J3");
+        st.display();
+        st1.display();
+    }
+    
+}
