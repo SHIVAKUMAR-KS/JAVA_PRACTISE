fd13fe08f056681db164e274abf5abf66546060f
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-10-18 21:52:41 +0530

    done with ShowRoom.java

--- JAVA/OOPS/Car_Management_ShowRoom/ShowRoom.java
+++ JAVA/OOPS/Car_Management_ShowRoom/ShowRoom.java
@@ -22,5 +22,17 @@ public class ShowRoom implements utility{
     @Override
     public void setDetail() {
         Scanner sc=new Scanner(System.in);
+        System.out.println("******Welcome to Car ShowRoom********");
+        System.out.println();
+        System.out.print("Enter ShowRoom Name:");
+        ShowRoom_Name= sc.next();
+        System.out.print("Enter ShowRoom Address :");
+        ShowRoom_Address=sc.next();
+        System.out.println("Enter the Manager Name :");
+        manager_name=sc.next();
+        System.out.print("Total Number of employee in showroom:");
+        total_Employe=sc.nextInt();
+        System.out.print("Total Number of car avalaible in stock");
+        total_car_in_Stock=sc.nextInt();
     }
 }
