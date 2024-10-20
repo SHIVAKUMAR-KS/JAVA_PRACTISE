de6811116cc691a6b0eb0dbacf1f866cdd4693e2
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-10-20 07:48:06 +0530

    done with other classes

--- JAVA/OOPS/Car_Management_ShowRoom/Employee.java
+++ JAVA/OOPS/Car_Management_ShowRoom/Employee.java
@@ -1,10 +1,14 @@
 package Car_Management_ShowRoom;
 
+import java.util.Scanner;
+import java.util.UUID;
+
 public class Employee implements utility{
     String emp_id;
     String emp_name;
     String emp_age;
     String emp_department;
+    String showRoom;
 
     @Override
     public void getDetail() {
@@ -12,10 +16,23 @@ public class Employee implements utility{
         System.out.println("Name:"+emp_name);
         System.out.println("Age:"+emp_age);
         System.out.println("Dapartment:"+emp_department);
+        System.out.println("ShowRoom Name:"+showRoom);
     }
     @Override
     public void setDetail() {
-
+        Scanner sc=new Scanner(System.in);
+        UUID uuid=UUID.randomUUID();
+        emp_id=String.valueOf(uuid);
+        System.out.println("*********Enter the Employee Details:**********");
+        System.out.println();
+        System.out.print("Enter Employee Name :");
+        emp_name= sc.nextLine();
+        System.out.print("Enter Employee Age:");
+        emp_age=sc.nextLine();
+        System.out.print("Enter Employee's Department:");
+        emp_department=sc.nextLine();
+        System.out.print("Enter ShowRoom Name:");
+        showRoom=sc.nextLine();
     }
 
 
--- JAVA/OOPS/Car_Management_ShowRoom/main.java
+++ JAVA/OOPS/Car_Management_ShowRoom/main.java
@@ -1,11 +1,12 @@
 package Car_Management_ShowRoom;
 
 interface utility{
-    public void getDetail();
+    public  void  getDetail();
     public  void setDetail();
 }
 public class main {
+
     public static void main(String[] args) {
-        System.out.println("Ss");
+
     }
 }
