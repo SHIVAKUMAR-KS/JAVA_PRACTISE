4820219eaee250f1df86b1b28b1997ac3cad9c53
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-12-30 12:21:04 +0530

    added new code for pattern

--- JAVA/OOPS/Problems/AsListUse.java
+++ JAVA/OOPS/Problems/AsListUse.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class AsListUse {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-      //  String str[]=new String[]{"shiva,ayush,raju,kira"};
-        String str="shiva";
+       String str[]=new String[]{"shiva,ayush,raju,kira"};
+        //String str="shiva";
 
         List<String> list= Arrays.asList(str);
         System.out.println("Strign is converted into list:"+list);
