67546561630a8d2b368d4ef931eac55cbca969aa
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-12-30 12:28:10 +0530

    added new

--- JAVA/OOPS/Pattern_Printing/main.java
+++ JAVA/OOPS/Pattern_Printing/main.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the n value");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                System.out.print("*"+" ");
+                //System.out.print(j+" ");
+                //System.out.print(i+
+                //System.out.print(j+64+" ");
+                //System.out.print((char)(i+64)+" ");
+                //System.out.print((char)(j+64)+" ");//upper case
+                System.out.print((char)(j+96)+" ");//lower case
             }
             System.out.println();
         }
